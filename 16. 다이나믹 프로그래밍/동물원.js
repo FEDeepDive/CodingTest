9372f82203eb7bc2c4c19bb8900703c7861b914c
devkyoung2 <devkyoung2@gmail.com>
2025-04-17 00:06:35 +0900

    [ PS ] : 1309

---
+++ 16. 다이나믹 프로그래밍/동물원.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const n = +fs.readFileSync(path).toString().trim();
+const dp = Array.from({ length: n + 1 }, () => Array(3).fill(0));
+const mod = 9901;
+
+dp[1] = [1, 1, 1];
+
+// n마리의 사자를 배치하는 경우
+// 왼쪽, 오른쪽, 배치 X
+for (let i = 2; i <= n; i++) {
+  dp[i][0] = (dp[i - 1][1] + dp[i - 1][2]) % mod;
+  dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % mod;
+  dp[i][2] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % mod;
+}
+
+console.log(dp[n].reduce((pre, cur) => pre + cur) % mod);
