05030ee58c584b7b13be152905a0d0e8cbc1040d
devkyoung2 <devkyoung2@gmail.com>
2025-07-20 18:25:31 +0900

    [PS] : 1019

---
+++ 18. 수학/책 페이지.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+let n = +fs.readFileSync(path).toString().trim();
+const numbers = Array(10).fill(0);
+let digit = 1;
+let add = 0;
+
+while (n > 0) {
+  const rest = n % 10;
+  n = Math.floor(n / 10);
+
+  for (let i = 0; i <= 9; i++) {
+    if (i < rest) numbers[i] += (n + 1) * digit;
+    else if (i === rest) numbers[i] += n * digit + add + 1;
+    else numbers[i] += n * digit;
+  }
+
+  add += rest * digit;
+  numbers[0] -= digit;
+  digit *= 10;
+}
+
+console.log(...numbers);
